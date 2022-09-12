5736c2aa3b09ccf96de2ff10582c4d7f981aa229
bytecod31 <emwiti658@gmail.com>
2022-09-12 18:43:08 +0300

    created: motor functions

--- sesboard-motor-shield/lib/sesmotor/sesmotor.cpp
+++ sesboard-motor-shield/lib/sesmotor/sesmotor.cpp
@@ -1,24 +1,56 @@
 #include "sesmotor.h"
 
-Sesmotor::Sesmotor(bool display_msg){
-    // Anything you need when instantiating your object is put here
-    _msg = display_msg;
+Sesmotor::Sesmotor(uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4, uint8_t ena, uint8_t enb){
+    /*
+     * Init the motor variables
+     */
+    this->in1 = in1;
+    this->in2 = in2;
+    this->in3 = in3;
+    this->in4 = in4;
+    this->ena = ena;
+    this->enb = enb;
+
+    // initialize al motors off
+    this->init();
 }
 
-// initialize the serial monitor
-void Sesmotor::begin(int baud_rate) {
-    Serial.begin(baud_rate);
-    if(_msg){
-        Serial.println("Object created success");
-    }
+void Sesmotor::init(uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4){
+    /*
+     * Set all pins as output
+     * Turn the motor object off initially
+     */
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
+
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+}
+
+void Sesmotor::forward(uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4) {
+    /*
+     * Move the motor in the forward direction
+     * This function is for avr boards
+     */
+    digitalWrite(in1, 1);
+    digitalWrite(in2, 0);
+    digitalWrite(in3, 1);
+    digitalWrite(in4, 0);
 
 }
 
-long Sesmotor::getRandomNumber(){
-    unsigned  long specialNumber = random(5, 1000);
+void Sesmotor::reverse(uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4) {
+    /*
+     * Move the motor in the forward direction
+     * This function is for avr boards
+     */
+    digitalWrite(in1, 0);
+    digitalWrite(in2, 1);
+    digitalWrite(in3, 0);
+    digitalWrite(in4, 1);
 
-    specialNumber *= PI;
-    specialNumber -= 5;
-
-    return specialNumber;
 }
--- sesboard-motor-shield/lib/sesmotor/sesmotor.h
+++ sesboard-motor-shield/lib/sesmotor/sesmotor.h
@@ -15,11 +15,17 @@
 class Sesmotor {
     public:
         // Constructor
-        Sesmotor(bool display_msg=false);
+        Sesmotor(uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4, uint8_t ena, uint8_t enb);
 
         // methods
+        void init();
         void begin(int baud_rate = 9600);
-        long getRandomNumber();
+        void forward(int in1, int in2, int in3, int in4);
+        void reverse();
+        void on();
+        void off();
+        void turnLeft();
+        void turnRight();
 
     private:
         bool _msg;
